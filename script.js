46354173b804c8c704c7f2f5904bed5adbf12ff9
Lucasfs <lucasfernandesfs@gmail.com>
2022-11-11 20:29:30 -0300

    Arquivos atualizados

---
+++ script.js
@@ -0,0 +1,22 @@
+function insert(num){
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num;
+}
+function limpar(){
+    document.getElementById('res').innerHTML = '';
+}
+function somar(){
+
+}
+function voltar(){
+    var res = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1)
+}
+function calcular(){
+    var res = document.getElementById('res').innerHTML;
+    if (res){
+        document.getElementById('res').innerHTML = eval(res);
+    }else{
+        document.getElementById('res').innerHTML = 'Nada para calcular'
+    }
+}
